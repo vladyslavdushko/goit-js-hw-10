534ac7e16ecd11e96097bf7071e084804a542623
vladyslavdushko <110049102+vladyslavdushko@users.noreply.github.com>
2024-06-06 13:46:21 +0000

    Deploying to gh-pages from @ vladyslavdushko/goit-js-hw-10@90f1f85b2af3587de6da6659f7a3256360985af2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,30 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as h,i as y}from"./assets/vendor-77e16229.js";const r=document.querySelector("#datetime-picker"),e=document.querySelector("[data-start]"),o=document.querySelector(".timer");let l=null;const L={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){if(t[0]-Date.now()<0)return e.classList.remove("active"),e.classList.add("inactive"),y.error({title:"Error",message:"Please choose a date in the future"});e.classList.remove("inactive"),e.classList.add("active"),l=t[0]}};h(r,L);function s(t){const n=Math.floor(t/864e5),m=Math.floor(t%864e5/36e5),f=Math.floor(t%864e5%36e5/6e4),v=Math.floor(t%864e5%36e5%6e4/1e3);return{days:n,hours:m,minutes:f,seconds:v}}e.addEventListener("click",C);function a(t){return String(t).padStart(2,"0")}function C(){if(!e.classList.contains("active"))return;const t=o.querySelector("[data-days]"),i=o.querySelector("[data-hours]"),c=o.querySelector("[data-minutes]"),u=o.querySelector("[data-seconds]");e.classList.remove("active"),e.classList.add("inactive"),r.disabled=!0;let d=setInterval(()=>{r.ariaDisabled=!0;let n=l-Date.now();if(t.textContent=a(s(n).days),i.textContent=a(s(n).hours),c.textContent=a(s(n).minutes),u.textContent=a(s(n).seconds),n<=0)return t.textContent="00",i.textContent="00",c.textContent="00",u.textContent="00",e.classList.remove("inactive"),e.classList.add("active"),r.disabled=!1,clearInterval(d)},1e3)}
+import{i as a,S as c}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const n of e)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const n={};return e.integrity&&(n.integrity=e.integrity),e.referrerPolicy&&(n.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?n.credentials="include":e.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function r(e){if(e.ep)return;e.ep=!0;const n=s(e);fetch(e.href,n)}})();const u="44111421-e553a4c4acf6f519f7af5aa9e",d="https://pixabay.com/api/";async function m(o){const t={origin:d,key:u,image_type:"photo",q:o,orientation:"horizontal",safesearch:!0},s=`${t.origin}?key=${t.key}&q=${t.q}&image_type=${t.image_type}&orientation=${t.orientation}&safesearch=${t.safesearch}`;return fetch(s).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()}).then(r=>r.total===0?(a.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),null):r).catch(r=>(a.error({title:"Error",message:r.message}),null))}function l(o){const t=document.querySelector(".gallery"),s=document.querySelector(".loader-container");s.classList.add("is-visible"),s.classList.remove("is-hidden"),t.innerHTML="",o.then(r=>{let e=r.hits.map(i=>`
+                <li class='gallery-item'>
+                <a href=${i.largeImageURL}>
+                <img src=${i.webformatURL} alt=${i.tags} class="gallery-img" title="${i.tags}">
+                <div class="inner-container">
+                <ul class="inner-item-ul">
+                <li class="inner-item-li">
+                    <p class="inner-item-p">Likes</p>
+                    <span class="inner-item-span">${i.likes}</span>
+                </li>
+
+                <li class="inner-item-li">
+                    <p class="inner-item-p">Views</p>
+                    <span class="inner-item-span">${i.views}</span>
+                </li>
+
+                <li class="inner-item-li">
+                    <p class="inner-item-p">Comments</p>
+                    <span class="inner-item-span">${i.comments}</span>
+                </li>
+
+                <li class="inner-item-li">
+                    <p class="inner-item-p">Downloads</p>
+                    <span class="inner-item-span">${i.downloads}</span>
+                </li>
+                </ul>
+                </div>
+                </a> 
+                </li>`).join("");t.insertAdjacentHTML("beforeend",e),new c(".gallery a",{captionDelay:250}).refresh(),s.classList.remove("is-visible"),s.classList.add("is-hidden")}).catch(r=>{throw s.classList.remove("is-visible"),s.classList.add("is-hidden"),new Error(r)})}const p=document.querySelector("button"),f=document.querySelector("form");document.querySelector(".gallery");document.querySelector(".loader-container");p.addEventListener("click",o=>{o.preventDefault();const t=f.elements.q.value.trim(),s=m(t);if(t==="")return alert("Enter text");s&&(l(s),console.log(typeof l))});
 //# sourceMappingURL=commonHelpers.js.map
